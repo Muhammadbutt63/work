781f26bc9e8d4de4ab55afcc4dfeab938ae3eb8b
Muhammadbutt63 <muhammadbutt637@gmail.com>
2023-02-22 21:13:57 +0500

    add

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("mybutton").addEventListener("click", function() {
+    window.location.href = "login.html";
+  });
+
+  document.getElementById("b2").addEventListener("click", function() {
+    window.location.href = "signup.html";
+  });
